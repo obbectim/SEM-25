78c751ece5f40b80cbac613dc8c706f91da393ac
pimveldhuisen <pim.veldhuisen@gmail.com>
2015-09-11 20:57:13 +0200

    Fix stuff that went wrong in merge conflicts

--- src/test/java/nl/tudelft/bejeweled/board/ValidMovesTest.java
+++ src/test/java/nl/tudelft/bejeweled/board/ValidMovesTest.java
@@ -2,7 +2,6 @@ package nl.tudelft.bejeweled.board;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -13,19 +12,17 @@ import nl.tudelft.bejeweled.sprite.SpriteStore;
 
 /**
  * Test if certain moves are valid.
- * 
  * @author Pim Veldhuisen
  */
 
 public class ValidMovesTest {
-	private static final int GRID_WIDTH = 8;
-	private static final int GRID_HEIGHT = 8;
-	private static final int SPRITE_WIDTH = 64;
-	private static final int SPRITE_HEIGHT = 64;
-		
 	private BoardFactory boardFactory;
 	private javafx.scene.Group mockGroup;
 	
+	
+	/**
+	 * Creates the board to test with.
+	 */
 	@Before
 	public void setUp() {
 		SpriteStore mockSpriteStore = mock(SpriteStore.class);
@@ -35,24 +32,32 @@ public class ValidMovesTest {
 
 	}
 	
-		
+	/**
+	 * Test out of moves if there are no moves.
+	 */	
 	@Test
 	public void verifyNoValidMoves() {
-		Board board = boardFactory.fromTextGenerateBoard("/boards/NoMoves.txt", mockGroup, GRID_WIDTH, GRID_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT);
+		Board board = boardFactory.fromTextGenerateBoard("/boards/NoMoves.txt", mockGroup);
 		assertTrue(board.outOfMoves());
 
 	}
 	
+	/**
+	 * Test not out of moves if there is a combo on the board.
+	 */	
 	@Test
 	public void verifyCombosOnBoard() {
-		Board board = boardFactory.fromTextGenerateBoard("/boards/1LineOf3.txt", mockGroup, GRID_WIDTH, GRID_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT);
+		Board board = boardFactory.fromTextGenerateBoard("/boards/1LineOf3.txt", mockGroup);
 		assertFalse(board.outOfMoves());
 
 	}
 	
+	/**
+	 * Test not out of moves if there is no combo on the board, but there is a move possible.
+	 */	
 	@Test
 	public void verifyNoCombosButMoves() {
-		Board board = boardFactory.fromTextGenerateBoard("/boards/NoCombosButMoves.txt", mockGroup, GRID_WIDTH, GRID_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT);
+		Board board = boardFactory.fromTextGenerateBoard("/boards/NoCombosButMoves.txt", mockGroup);
 		assertFalse(board.outOfMoves());
 
 	}
