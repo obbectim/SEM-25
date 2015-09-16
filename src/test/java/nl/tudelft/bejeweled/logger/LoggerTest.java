8a482e5a20fc703beed1b115813174976e55f6fa
Jan Harms <jan.gerrit.harms@gmail.com>
2015-09-16 14:15:33 +0200

    Increased test coverage

--- src/test/java/nl/tudelft/bejeweled/logger/LoggerTest.java
+++ src/test/java/nl/tudelft/bejeweled/logger/LoggerTest.java
@@ -12,11 +12,15 @@ import org.junit.Test;
 
 public class LoggerTest {
 
+	/**
+	 * Test the info logging function.
+	 * @throws IOException Exception when the file is not available
+	 */
 	@Test
 	public void testInfoLogging() throws IOException {
 		Logger.enable();
 		// Create a logFile
-		Logger.log("INFO ", "Hello World!");
+		Logger.logInfo("Hello World!");
 		Path logFilePath = Logger.getLogFilePath();
 		Logger.close();
 
@@ -28,6 +32,54 @@ public class LoggerTest {
 		assertTrue(lines.size() > 0 && lines.size() < 3);
 		assertEquals("INFO", firstLine.substring(0, 4));
 		assertEquals("Hello World!", firstLine.substring(firstLine.length()-12, firstLine.length()));
+		
+		Files.delete(logFilePath);
+	}
+	
+	/**
+	 * Test the error logging function.
+	 * @throws IOException Exception when the file is not available
+	 */
+	@Test
+	public void testErrorLogging() throws IOException {
+		Logger.enable();
+		// Create a logFile
+		Logger.logError("Hakuna Matata");
+		Path logFilePath = Logger.getLogFilePath();
+		Logger.close();
+
+	    List<String> lines = Files.lines(logFilePath).collect(
+	            Collectors.toList());
+		
+	    String firstLine = lines.get(0);
+	    
+		assertTrue(lines.size() > 0 && lines.size() < 3);
+		assertEquals("ERROR", firstLine.substring(0, 5));
+		assertEquals("Hakuna Matata", firstLine.substring(firstLine.length()-13, firstLine.length()));
+		
+		Files.delete(logFilePath);
+	}
+	
+	/**
+	 * Test the error warning function.
+	 * @throws IOException Exception when the file is not available
+	 */
+	@Test
+	public void testWarningLogging() throws IOException {
+		Logger.enable();
+		// Create a logFile
+		Logger.logWarning("Hello JavaFX");
+		Path logFilePath = Logger.getLogFilePath();
+		Logger.close();
+
+	    List<String> lines = Files.lines(logFilePath).collect(
+	            Collectors.toList());
+		
+	    String firstLine = lines.get(0);
+	    
+		assertTrue(lines.size() > 0 && lines.size() < 3);
+		assertEquals("WARNING", firstLine.substring(0, 7));
+		assertEquals("Hello JavaFX", firstLine.substring(firstLine.length()-12, firstLine.length()));
 		
 		Files.delete(logFilePath);
 	}
