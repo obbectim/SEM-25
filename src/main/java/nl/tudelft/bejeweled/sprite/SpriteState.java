bcf70586c7528f23d27da52b138d4628081480a1
pimveldhuisen <pim.veldhuisen@gmail.com>
2015-09-25 17:42:25 +0200

    Add the actual enum mentioned previously <logic

---
+++ src/main/java/nl/tudelft/bejeweled/sprite/SpriteState.java
@@ -0,0 +1,12 @@
+package nl.tudelft.bejeweled.sprite;
+
+/**
+ * Created by Pim on 22-9-2015.
+ * Enum for different Sprite states.
+ */
+
+public enum SpriteState {
+	IDLE,
+	ANIMATION_ACTIVE,
+	TO_BE_REMOVED
+}
